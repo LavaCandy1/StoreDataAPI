73c7ec32744258bf89168181dc7687514a4cdfd3
LavaCandy1 <aayush.garg.1793@gmail.com>
2025-09-09 19:15:25 +0530

    first commit

---
+++ src/main/java/com/AyushGarg/StoreDataAPI/StoreDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.AyushGarg.StoreDataAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AyushGarg/StoreDataAPI/StoreDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AyushGarg.StoreDataAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
