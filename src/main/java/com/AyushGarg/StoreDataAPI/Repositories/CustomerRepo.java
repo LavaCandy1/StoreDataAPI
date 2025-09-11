f6c37fdfc10e6e97212c5966270ec39100e7e850
LavaCandy1 <aayush.garg.1793@gmail.com>
2025-09-11 17:47:27 +0530

    minor rename

--- src/main/java/com/AyushGarg/StoreDataAPI/Repositories/CustomerRepo.java
+++ src/main/java/com/AyushGarg/StoreDataAPI/Repositories/CustomerRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Customer extends JpaRepository<Customer,Long>{
+public interface CustomerRepo extends JpaRepository<CustomerRepo,Long>{
     
 }
