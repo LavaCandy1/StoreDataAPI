b4464b6860162b38cad4f3d5f5d48482ebfffe28
LavaCandy1 <aayush.garg.1793@gmail.com>
2025-09-12 19:17:56 +0530

    redundant imports removed

--- src/main/java/com/AyushGarg/StoreDataAPI/Service/IngestionService/OrderDataIngestionService.java
+++ src/main/java/com/AyushGarg/StoreDataAPI/Service/IngestionService/OrderDataIngestionService.java
@@ -1,9 +1,7 @@
 package com.AyushGarg.StoreDataAPI.Service.IngestionService;
 
 import java.sql.Date;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.core.ParameterizedTypeReference;
