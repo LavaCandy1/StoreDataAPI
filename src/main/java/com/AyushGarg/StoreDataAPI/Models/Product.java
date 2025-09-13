7dc1bc11088d80febe500ce26eccb3757e936e37
LavaCandy1 <aayush.garg.1793@gmail.com>
2025-09-13 11:07:30 +0530

    added swagger UI and set base url /api

--- src/main/java/com/AyushGarg/StoreDataAPI/Models/Product.java
+++ src/main/java/com/AyushGarg/StoreDataAPI/Models/Product.java
@@ -1,17 +1,13 @@
 package com.AyushGarg.StoreDataAPI.Models;
 
-import java.sql.Date;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
